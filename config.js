56f1450bd34d596234b8365b5f05df599699dd7f
writen <sarnowsky94@gmail.com>
2017-09-30 22:32:05 +0200

    Create config file

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    GAME_TITLE: 'Phaser Community Edition Boilerplate', // Text for title tag in index.html
+    BUILD_PATH: './build', // Destination folder
+    OUTPUT_FILE: 'game.js', // File with generated bundle
+    MAIN_FILE: './src/index.js', // Main file (entry point)
+    PHASER_PATH: './node_modules/phaser-ce/build/phaser.min.js', // Path to Phaser-ce library
+    ASSETS_PATH: './src/assets', // Folder with your asset files
+    INDEX_TEMPLATE_FILE: './src/index.ejs' // Template file of your index.html
+};

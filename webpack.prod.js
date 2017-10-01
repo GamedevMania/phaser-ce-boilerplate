33127421b0fe8d98c62e73ec0868342322346a06
writen <sarnowsky94@gmail.com>
2017-10-01 03:48:18 +0200

    Create webpack config file for production

---
+++ webpack.prod.js
@@ -0,0 +1,13 @@
+const CONFIG = require('./config');
+const merge = require('webpack-merge');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+    devtool: 'source-map',
+    plugins: [
+        new UglifyJSPlugin({
+            sourceMap: true
+        })
+    ]
+});
